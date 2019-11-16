631f666a7aba9fa9c1e97b0af1f678e9ae33c032
winDX543 <55429489+winDX543@users.noreply.github.com>
2019-11-16 14:14:19 +0630

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@
 				{
 					"type":"web_url",
 					"title":"Visit Page",
-					"url":"https://web.facebook.com/%E1%80%80%E1%80%B0%E1%82%90%E1%80%BD%E1%80%AC%E1%80%96%E1%80%90%E1%80%B9-113762300024095/?__tn__=kC-R&eid=ARAblm5cCwsOgVy6pTAl96nSWBS8BGnOOLZd_KIoKbhcyomoY-aa1r4dZZlNYZlbYLsroXaRxEKr2KYR&hc_ref=ARRfMuUvyQoHOHUX0oSIkkv1W_5kRQDyfNzbqYnvvwICjSImHpIKdv9TQITzWl87eHk&fref=nf&__xts__[0]=68.ARAel5l8bZYRJTnNAN4rbgAdPzZv8s4SBin-gZ1H7wYDWQ8B3ZHDkcJ7YqMRbadqT8lqE6Ex00Sx6wxDmzs4QSPTWtw8BQJgU9sw21q5MtfDUgpxfnVFxP708BzRevrwE8S1x7dkmWECnGFFIDO4wfpxoknxENDjP2oB89LydIeiR-XH2id4FTcBoJUmGfWngRocZFDp0Sk9Aox1Myopt0DIqLQZhVcn9Q8JMdc584Eb6Tp347dRdI5KcUKjP285sNbMYmw1bM8aZXzpSWwF5z8E-tbMgiVetCha9XWxxiJBFg4N5y84McYJBd2Fh19Y0if-OBRiFkqXj75OdiDb320",
+					"url":"https://mym-acavxb.firebaseapp.com/index.html",
 					"webview_height_ratio":"tall"
 
 				}
@@ -113,13 +113,9 @@ app.post('/webhook', (req, res) => {
 
 	}}
 	 if(userButton == 'Hi' || userComment == 'Hi'){
+	 }
+	}
 
-
-/*	MessengerExtensions.askPermission(
-	funcition(permission_response) {
-	// Person grants or rejects the asked permission.
-	let permission */
-	MessengerExtensions.askPermission(success, error, permission);
  
 requestify.post(sendmessageurl,
 {        
@@ -214,3 +210,9 @@ console.log('error', error);
   }
 
 });
+
+
+
+
+
+
